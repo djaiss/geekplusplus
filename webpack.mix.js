bc826a069a0730efbfc70d902ad8a45b22136c75
Regis Freyd <djaiss@users.noreply.github.com>
2020-11-08 11:05:04 -0500

    chore: better css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 require('mix-tailwindcss');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,8 +14,18 @@ require('mix-tailwindcss');
  |
  */
 
+let purgeCssOptions = {
+    enabled: true,
+    // List of regex of CSS class to not remove
+    whitelistPatterns: [/^ball-pulse/, /^vs__/, /dot-/],
+    // List of regex of CSS class name whose child path CSS class will not be removed
+    //  ex: to exclude "jane" in "mary jane": add "mary")
+    whitelistPatternsChildren: [/^vs-/],
+};
+
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .purgeCss(purgeCssOptions)
     .options({
         processCssUrls: false,
         postCss: [tailwindcss('./tailwind.config.js')],
